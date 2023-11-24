e53081ad26af4bacd1a87d753dc6934f97cb8a2c
ANDERSON DANIEL CHAVEZ V <daniel.chavez@imagineapps.co>
2023-11-23 22:42:23 -0500

    connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        /* tus entidades aquí */
+      ],
+      synchronize: true, // en desarrollo; para producción, configúralo como false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
